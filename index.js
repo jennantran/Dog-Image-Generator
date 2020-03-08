1eb452285655c537d22f128ba22fb749aa5c03e3
Jennifer Tran <jennantran@gmail.com>
2020-03-07 21:15:32 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function getDogImages(){
+    let action_src = $("#number").val();
+    let urlLink = "https://dog.ceo/api/breeds/image/random/";
+    let fullUrlLink = urlLink + action_src;
+
+    fetch(fullUrlLink)
+    .then(response => response.json())
+    .then(responseJson => 
+        displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'));
+
+
+      reload();
+      
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    const el = $('.results');
+    for (let i = 0; i < responseJson.message.length; i++){
+        //constructing html with the current image index
+        let img = `<img src="${responseJson.message[i]}" class="results-img">`;
+        //append the html to the element
+        el.append(img);
+    }
+      //display the results section
+      $('.results').removeClass('hidden');
+    }
+
+function watchForm(){
+    $('form').submit(event => {
+        event.preventDefault();
+        getDogImages();
+    });
+}
+
+
+$(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+  });
